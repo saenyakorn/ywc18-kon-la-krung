088ea782d80f78ef9fa13ea5df1f699ce61136fc
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-11-20 02:02:44 +0700

    complete: loading screen

--- src/core/components/card/cardList.component.tsx
+++ src/core/components/card/cardList.component.tsx
@@ -1,20 +1,29 @@
-import { Card, Divider, Space, Typography } from "antd"
 import React from "react"
-
-import BadgeComponent from "../badge.component"
+import { Card, Divider, Space, Typography } from "antd"
+import { LoadingOutlined } from "@ant-design/icons"
 
 import "./cardList.style.less"
 import FacilityIconComponent from "../icons/facilityIcon.component"
 import { FacilityIconProps } from "../icons/facilityIcon.component"
 import { useSearchContext } from "../../controllers/search.controller"
 import SkeletonImageComponent from "../skeletonImage.component"
+import BadgeComponent from "../badge.component"
+import { useDataCenterContext } from "../../controllers/dataCenter.controller"
 
 const { Title, Text } = Typography
 
 export interface CardListProps {}
 
 export default function CardListComponent() {
+  const { isReady } = useDataCenterContext()
   const { currentMerchants } = useSearchContext()
+
+  if (!isReady)
+    return (
+      <div className="loading-icon">
+        <LoadingOutlined />
+      </div>
+    )
 
   return (
     <Space direction="vertical" className="space-container">
--- src/core/components/optionBar.component.tsx
+++ src/core/components/optionBar.component.tsx
@@ -1,17 +1,26 @@
 import { Card } from "antd"
 import React from "react"
+import { LoadingOutlined } from "@ant-design/icons"
 
 import "./optionBar.style.less"
 
 // constants
 import OptionComponent from "./option.component"
+import { useDataCenterContext } from "../controllers/dataCenter.controller"
 
 export interface OptionBarProps {}
 
 export default function OptionBarComponent() {
+  const { isReady } = useDataCenterContext()
   return (
     <Card className="sidebar">
-      <OptionComponent />
+      {isReady ? (
+        <OptionComponent />
+      ) : (
+        <div className="loading-icon">
+          <LoadingOutlined />
+        </div>
+      )}
     </Card>
   )
 }
