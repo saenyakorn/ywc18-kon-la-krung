37e3976ca92abcab590752b3f65e6591bd9f30e4
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-11-21 14:07:49 +0700

    feat: tooltip on facilities

--- src/core/components/icons/facilityIcon.component.tsx
+++ src/core/components/icons/facilityIcon.component.tsx
@@ -1,3 +1,4 @@
+import { Tooltip } from "antd"
 import React from "react"
 
 import "./facilityIcon.style.less"
@@ -16,57 +17,57 @@ export default function FacilityIconComponent({ facility }: FacilityIconProps) {
   switch (facility) {
     case "ที่จอดรถ":
       return (
-        <div className="icon">
+        <Tooltip placement="top" title={facility} className="icon">
           <img
             alt="car-icon"
             src="https://search-merchant.xn--42caj4e6bk1f5b1j.com/images/facilities/%E0%B8%97%E0%B8%B5%E0%B9%88%E0%B8%88%E0%B8%AD%E0%B8%94%E0%B8%A3%E0%B8%96.png"
           />
-        </div>
+        </Tooltip>
       )
     case "รับจองล่วงหน้า":
       return (
-        <div className="icon" style={{ paddingLeft: 6 }}>
+        <Tooltip placement="top" title={facility} className="icon" style={{ paddingLeft: 6 }}>
           <img
             alt="booking-icon"
             src="https://search-merchant.xn--42caj4e6bk1f5b1j.com/images/facilities/%E0%B8%A3%E0%B8%B1%E0%B8%9A%E0%B8%88%E0%B8%AD%E0%B8%87%E0%B8%A5%E0%B9%88%E0%B8%A7%E0%B8%87%E0%B8%AB%E0%B8%99%E0%B9%89%E0%B8%B2.png"
           />
-        </div>
+        </Tooltip>
       )
     case "สามารถนำสัตว์เลี้ยงเข้าได้":
       return (
-        <div className="icon">
+        <Tooltip placement="top" title={facility} className="icon">
           <img
             alt="pet-icon"
             src="https://search-merchant.xn--42caj4e6bk1f5b1j.com/images/facilities/%E0%B8%AA%E0%B8%B2%E0%B8%A1%E0%B8%B2%E0%B8%A3%E0%B8%96%E0%B8%99%E0%B8%B3%E0%B8%AA%E0%B8%B1%E0%B8%95%E0%B8%A7%E0%B9%8C%E0%B9%80%E0%B8%A5%E0%B8%B5%E0%B9%89%E0%B8%A2%E0%B8%87%E0%B9%80%E0%B8%82%E0%B9%89%E0%B8%B2%E0%B9%84%E0%B8%94%E0%B9%89.png"
           />
-        </div>
+        </Tooltip>
       )
     case "บริการจัดส่งอาหาร":
       return (
-        <div className="icon">
+        <Tooltip placement="top" title={facility} className="icon">
           <img
             alt="pet-icon"
             src="https://search-merchant.xn--42caj4e6bk1f5b1j.com/images/facilities/%E0%B8%9A%E0%B8%A3%E0%B8%B4%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%88%E0%B8%B1%E0%B8%94%E0%B8%AA%E0%B9%88%E0%B8%87%E0%B8%AD%E0%B8%B2%E0%B8%AB%E0%B8%B2%E0%B8%A3.png"
           />
-        </div>
+        </Tooltip>
       )
     case "จำหน่ายเครื่องดื่มแอลกอฮอล์":
       return (
-        <div className="icon">
+        <Tooltip placement="top" title={facility} className="icon">
           <img
             alt="pet-icon"
             src="https://search-merchant.xn--42caj4e6bk1f5b1j.com/images/facilities/%E0%B8%88%E0%B8%B3%E0%B8%AB%E0%B8%99%E0%B9%88%E0%B8%B2%E0%B8%A2%E0%B9%80%E0%B8%84%E0%B8%A3%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B8%87%E0%B8%94%E0%B8%B7%E0%B9%88%E0%B8%A1%E0%B9%81%E0%B8%AD%E0%B8%A5%E0%B8%81%E0%B8%AD%E0%B8%AE%E0%B8%AD%E0%B8%A5%E0%B9%8C.png"
           />
-        </div>
+        </Tooltip>
       )
     case "รับบัตรเครดิต":
       return (
-        <div className="icon">
+        <Tooltip placement="top" title={facility} className="icon">
           <img
             alt="pet-icon"
             src="https://search-merchant.xn--42caj4e6bk1f5b1j.com/images/facilities/%E0%B8%A3%E0%B8%B1%E0%B8%9A%E0%B8%9A%E0%B8%B1%E0%B8%95%E0%B8%A3%E0%B9%80%E0%B8%84%E0%B8%A3%E0%B8%94%E0%B8%B4%E0%B8%95.png"
           />
-        </div>
+        </Tooltip>
       )
   }
 }
