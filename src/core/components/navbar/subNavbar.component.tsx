1e568ba44a06d7524d02ecc91ca588d72ce17bba
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-11-21 14:04:10 +0700

    add: link on หน้าแรก

--- src/core/components/navbar/subNavbar.component.tsx
+++ src/core/components/navbar/subNavbar.component.tsx
@@ -8,8 +8,12 @@ export interface SubNavbarProps {}
 export default function SubNavbarComponent() {
   return (
     <div className="navbar-container bg-blue">
-      <Breadcrumb className="navbar-item ">
-        <Breadcrumb.Item>หน้าแรก</Breadcrumb.Item>
+      <Breadcrumb className="navbar-item">
+        <Breadcrumb.Item>
+          <a href="https://www.xn--42caj4e6bk1f5b1j.com/" target="_blank" rel="noreferrer">
+            หน้าแรก
+          </a>
+        </Breadcrumb.Item>
         <Breadcrumb.Item>ค้นหา</Breadcrumb.Item>
       </Breadcrumb>
     </div>
